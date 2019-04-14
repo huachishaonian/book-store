075826c3a79d2e25f129bbff23fdd4cad4317134
wangzipeng <wangzipeng1996@aliyun.com>
2019-04-14 21:12:28 +0800

    vuex

--- src/store.js
+++ src/store.js
@@ -5,10 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    username: '',
   },
   mutations: {
-
+    setUserName (state, username) {
+      state.username = username;
+    },
+    clearUserName (state) {
+      state.username = '';
+    },
   },
   actions: {
 
