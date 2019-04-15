c9a0865bcfcc9d1136743b50065764605fd4b127
wangzipeng <wangzipeng1996@aliyun.com>
2019-04-15 18:08:24 +0800

    ...

--- src/store.js
+++ src/store.js
@@ -15,7 +15,7 @@ export default new Vuex.Store({
     },
     clearUserName (state) {
       state.username = '';
-      // state.isRoot = false;
+      state.isRoot = false;
     },
   },
   actions: {
